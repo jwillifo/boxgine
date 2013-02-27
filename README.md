27ad68826473b668413faf6a1bab6ae668d9993e
John Williford <willifordJohnM@gmail.com>
2013-02-26 22:52:53 -0500

    adds new isometric wall width and asynchronous html5 range inputs

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
 #boxgine
-Boxgine is a script for generating art out of ascii box characters.
+Boxgine is a script for generating art out of ascii box characters in a text grid and now 
+isometric pieces on an html canvas.
+
+##Demo
+[http://www.johnwilliford.com/boxgine.html](Boxgine)
 
 ##About
-It started out as a wpf application I made after staring at my BIOS
-too long. After it was sitting around unused for a couple months I started porting it
-to different languages for practice.
+It started out as a wpf application I made after staring at my BIOS too long. After it was 
+sitting around unused for a couple months I started porting it to different languages for 
+practice.
 
 ##Author
 John Williford
--- inc/boxgine.js
+++ inc/boxgine.js
@@ -2,6 +2,13 @@ var maMod = new Array();
 var numBoxes = 3;
 var cW = 64;
 var cH = 28;
+/*Dictionary to help perform letter math of adding 
+and subtracting overlapping pieces.
+
+Pieces defined as string of top/right/bottom/left
+0 = blank space
+L = regular LINE
+B = BOLD line*/
 var dict = {};
     dict["0000"] = " ";
     dict["XXXX"] = "▓";
@@ -10,7 +17,7 @@ var dict = {};
     dict["B0B0"] = "║";
     dict["L0L0"] = "│";
 
-    //top left corner picked at random
+    //top left corner is picked at random
     dict["0LL0"] = "┌";
     dict["0BL0"] = "╒";
     dict["0LB0"] = "╓";
@@ -26,12 +33,13 @@ var dict = {};
     dict["BL00"] = "╙";
     dict["BB00"] = "╚";
 
-    //bottom right corner picked at random
+    //bottom right corner is picked at random
     dict["L00L"] = "┘";
     dict["L00B"] = "╛";
     dict["B00L"] = "╜";
     dict["B00B"] = "╝";
 
+    //possible collisions
     dict["LLL0"] = "├";
     dict["LBL0"] = "╞";
     dict["BLB0"] = "╟";
@@ -57,14 +65,38 @@ var dict = {};
     dict["BLBL"] = "╫";
     dict["BBBB"] = "╬";
 
+    //4 possible tl and br pieces during random construction
     var tlArr = new Array("0LL0", "0BL0", "0LB0", "0BB0");
     var brArr = new Array("L00L", "L00B", "B00L", "B00B");
 
-function gen_btn_Click() {
+function clear_btn_Click() {
     document.getElementById('grid_txt').value = "";
+    document.getElementById("map").getContext("2d").clearRect(0, 0, document.getElementById("map").width, document.getElementById("map").height);
+}
+function redraw(changed, toChange) {
+    changed = document.getElementById(changed);
+    toChange = document.getElementById(toChange);
+    toChange.value = changed.value;
+
+    buildArr();
+}
+function gen_btn_Click() {
+    buildArr();
+}
+function buildArr() {
+    //reset form
+    document.getElementById('grid_txt').value = "";
+    var canvas = document.getElementById("map");
+    var context = canvas.getContext("2d");
+    var image = new Image();
+    image.src = "img/cube.png";
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    //input processing
     cW = parseInt(document.getElementById('charWidth_txt').value, 10);
     cH = parseInt(document.getElementById('charHeight_txt').value, 10);
     numBoxes = parseInt(document.getElementById('boxDepth_txt').value, 10);
+
     //initially filling an array of arrays with empty spaces
     maMod = [];
     for (var itr = 0; itr < cW; itr++){
@@ -74,14 +106,17 @@ function gen_btn_Click() {
         }
     }
 
+    //temp corners
     topLeft = "0000";
     topRight = "0000";
     botRight = "0000";
     botLeft = "0000";
+    //temp brushes used to draw sides
     topBrush = "0000";
     botBrush = "0000";
     leftBrush = "0000";
     rightBrush = "0000";
+
 
     rnd = Math.random();
     for (var ctr = 0; ctr < numBoxes; ctr++)
@@ -93,6 +128,8 @@ function gen_btn_Click() {
 
         topLeft = tlArr[randomFromTo(0,3)];
         botRight = brArr[randomFromTo(0,3)];
+
+        //once topLeft and botRight are found all else can be inferred
 
         topRight = setCharAt(topRight,3,topLeft.charAt(1));
         topRight = setCharAt(topRight,2,botRight.charAt(0));
@@ -113,11 +150,13 @@ function gen_btn_Click() {
         rightBrush = setCharAt(rightBrush,2,botRight.charAt(0));
 
         /*--------------------------------------------------*/
+        //inserting temp box into array
         maMod[pX][pY] = assign(maMod[pX][pY],topLeft);
         maMod[(pX + wid)][pY] = assign(maMod[(pX + wid)][pY],topRight);
         maMod[(pX + wid)][(pY + hei)] = assign(maMod[(pX + wid)][(pY + hei)],botRight);
         maMod[pX][(pY + hei)] = assign(maMod[pX][(pY + hei)],botLeft);
 
+        //painting on the sides 2 at a time
         headX = pX;
         headY = pY;
         while (headX < (pX + wid - 1))
@@ -135,7 +174,11 @@ function gen_btn_Click() {
         }
         /*--------------------------------------------------*/
     }
+
+    //characters in text grid display
     displayBox();
+    //isometric box display
+    renderIso(context,image);
 }
 function randomFromTo(from, to){
     return Math.floor(Math.random() * (to - from + 1) + from);
@@ -146,6 +189,7 @@ function setCharAt(str,index,chr) {
 
 }
 function assign(loc, val){
+    //assigns value in array and performs overlap math
     if (loc != "0000"){
         var temp = loc;
         for( i=0; i<4; i++) {
@@ -156,6 +200,7 @@ function assign(loc, val){
         return loc;
 
     }else{
+        //no overlap, regular assignment
         return val;
     }
 }
@@ -165,19 +210,58 @@ function displayBox() {
     for (vCtr = 0; vCtr < cH; vCtr++){
         for (hCtr = 0; hCtr < cW; hCtr++){
             if( dict[maMod[hCtr][vCtr]] != undefined){
+                //if it's a possible character within the limited set, draw
                 document.getElementById('grid_txt').value += dict[maMod[hCtr][vCtr]];
             }else{
+                //otherwise swap out all BOLD for regular LINE
+                //impossibles are: LBBB, BLBB, BBLB, BBBL, and the inverse
                 var temp = maMod[hCtr][vCtr];
                 temp = temp.replace(/B/g, 'L');
-
+                //put in the temp replacement
                 document.getElementById('grid_txt').value += dict[temp];
             }
         }
         if((vCtr + 1) < cH){
+            //carriage return
             document.getElementById('grid_txt').value += "\r";
         }
     }
 }
-function clear_btn_Click() {
-    document.getElementById('grid_txt').value = "";
+/*--------------------------------------------------*/
+function renderIso(ctxt,img) {
+    var rowX = document.getElementById("map").width / 2;
+    var rowY = 0;
+    var colX = document.getElementById("map").width / 2;
+    var colY = 0;
+
+    //hardprogrammed sprite dimensions !!!
+    cubeHalfWidth = 14;
+    cubeHalfHeight = 7;
+
+    for(e = 0; e < cH; e+=1){
+        rowX = colX
+        rowY = colY
+        for(i = 0; i < cW; i++){
+            if( maMod[i][e] != "0000"){
+
+                //only one wall thickness rendered
+                if(maMod[i][e].indexOf('B') === -1)
+                {
+                    img.src = "img/" + maMod[i][e] + ".png";
+                }else{
+                    img.src = "img/cube.png";
+                }
+
+                
+                //img.onload = function() {
+                    ctxt.drawImage(img, rowX, rowY);
+                //}
+            }
+
+            rowX += cubeHalfWidth;
+            rowY += cubeHalfHeight;
+        }
+        colX -= cubeHalfWidth;
+        colY += cubeHalfHeight;
+    }
 }
