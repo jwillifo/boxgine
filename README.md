396e90c3abbdd721f2c7738a6a896abfe9242098
John W <willifordJohnM@gmail.com>
2014-06-11 01:05:24 -0400

    prep for full sprite set

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Boxgine is a script for generating art out of ascii box characters in a text grid and now 
 isometric pieces on an html canvas.
 
-###[->Demo<-](http://www.johnwilliford.com/boxgine.php)
+###[->Demo<-](http://www.johnwilliford.com/boxgine/)
 
 ##About
 It started out as a wpf application I made after staring at my BIOS too long. After it was 
--- boxgine.js
+++ boxgine.js
@@ -64,26 +64,37 @@ var dict = {};
     dict["LBLB"] = "╪";
     dict["BLBL"] = "╫";
     dict["BBBB"] = "╬";
+    
+    //nonASCII collisions
+    dict["BLLL"] = "╫";
+    dict["LBLL"] = "╪";
+    dict["LLBL"] = "╫";
+    dict["LLLB"] = "╪";
+    
+    dict["LBBB"] = "╪";
+    dict["BLBB"] = "╫";
+    dict["BBLB"] = "╪";
+    dict["BBBL"] = "╫";
 
-    //4 possible tl and br pieces during random construction
-    var tlArr = new Array("0LL0", "0BL0", "0LB0", "0BB0");
-    var brArr = new Array("L00L", "L00B", "B00L", "B00B");
+//4 possible tl and br pieces during random construction
+var tlArr = new Array("0LL0", "0BL0", "0LB0", "0BB0");
+var brArr = new Array("L00L", "L00B", "B00L", "B00B");
 
-function clear_btn_Click() {
+function clear_btn_Click(){
     document.getElementById('grid_txt').value = "";
     document.getElementById("map").getContext("2d").clearRect(0, 0, document.getElementById("map").width, document.getElementById("map").height);
 }
-function redraw(changed, toChange) {
+function redraw(changed, toChange){
     changed = document.getElementById(changed);
     toChange = document.getElementById(toChange);
     toChange.value = changed.value;
-
+    
     buildArr();
 }
-function gen_btn_Click() {
+function gen_btn_Click(){
     buildArr();
 }
-function buildArr() {
+function buildArr(){
     //reset form
     document.getElementById('grid_txt').value = "";
     var canvas = document.getElementById("map");
@@ -91,21 +102,21 @@ function buildArr() {
     var image = new Image();
     image.src = "img/cube.png";
     context.clearRect(0, 0, canvas.width, canvas.height);
-
+    
     //input processing
     cW = parseInt(document.getElementById('charWidth_txt').value, 10);
     cH = parseInt(document.getElementById('charHeight_txt').value, 10);
     numBoxes = parseInt(document.getElementById('boxDepth_txt').value, 10);
-
+    
     //initially filling an array of arrays with empty spaces
     maMod = [];
-    for (var itr = 0; itr < cW; itr++){
+    for(var itr = 0; itr < cW; itr++){
         maMod[itr] = [];
         for(var etr = 0; etr < cH; etr++){
             maMod[itr][etr] = "0000";
         }
     }
-
+    
     //temp corners
     topLeft = "0000";
     topRight = "0000";
@@ -116,21 +127,20 @@ function buildArr() {
     botBrush = "0000";
     leftBrush = "0000";
     rightBrush = "0000";
-
-
+    
+    
     rnd = Math.random();
-    for (var ctr = 0; ctr < numBoxes; ctr++)
-    {
+    for(var ctr = 0; ctr < numBoxes; ctr++){
         wid = randomFromTo(1,(cW - 1));
         hei = randomFromTo(1,(cH -1));
         pX = randomFromTo(0,((cW -1)-wid));
         pY = randomFromTo(0,((cH -1)-hei));
-
+        
         topLeft = tlArr[randomFromTo(0,3)];
         botRight = brArr[randomFromTo(0,3)];
-
+        
         //once topLeft and botRight are found all else can be inferred
-
+        
         topRight = setCharAt(topRight,3,topLeft.charAt(1));
         topRight = setCharAt(topRight,2,botRight.charAt(0));
         
@@ -139,42 +149,40 @@ function buildArr() {
         
         topBrush = setCharAt(topBrush,1,topLeft.charAt(1));
         topBrush = setCharAt(topBrush,3,topLeft.charAt(1));
-
+        
         botBrush = setCharAt(botBrush,1,botRight.charAt(3));
         botBrush = setCharAt(botBrush,3,botRight.charAt(3));
-
+        
         leftBrush = setCharAt(leftBrush,0,topLeft.charAt(2));
         leftBrush = setCharAt(leftBrush,2,topLeft.charAt(2));
-
+        
         rightBrush = setCharAt(rightBrush,0,botRight.charAt(0));
         rightBrush = setCharAt(rightBrush,2,botRight.charAt(0));
-
+        
         /*--------------------------------------------------*/
         //inserting temp box into array
         maMod[pX][pY] = assign(maMod[pX][pY],topLeft);
         maMod[(pX + wid)][pY] = assign(maMod[(pX + wid)][pY],topRight);
         maMod[(pX + wid)][(pY + hei)] = assign(maMod[(pX + wid)][(pY + hei)],botRight);
         maMod[pX][(pY + hei)] = assign(maMod[pX][(pY + hei)],botLeft);
-
+        
         //painting on the sides 2 at a time
         headX = pX;
         headY = pY;
-        while (headX < (pX + wid - 1))
-        {
+        while(headX < (pX + wid - 1)){
             headX++;
             maMod[headX][pY] = assign(maMod[headX][pY],topBrush);
             maMod[headX][(pY + hei)] = assign(maMod[headX][(pY + hei)],botBrush);
         }
-
-        while (headY < (pY + hei - 1))
-        {
+        
+        while(headY < (pY + hei - 1)){
             headY++;
             maMod[pX][headY] = assign(maMod[pX][headY],leftBrush);
             maMod[(pX + wid)][headY] = assign(maMod[(pX + wid)][headY],rightBrush);
         }
         /*--------------------------------------------------*/
     }
-
+    
     //characters in text grid display
     displayBox();
     //isometric box display
@@ -183,38 +191,37 @@ function buildArr() {
 function randomFromTo(from, to){
     return Math.floor(Math.random() * (to - from + 1) + from);
 }
-function setCharAt(str,index,chr) {
+function setCharAt(str,index,chr){
     if(index > str.length-1) return str;
     return str.substr(0,index) + chr + str.substr(index+1);
 
 }
 function assign(loc, val){
     //assigns value in array and performs overlap math
-    if (loc != "0000"){
+    if(loc != "0000"){
         var temp = loc;
-        for( i=0; i<4; i++) {
-            if( temp.charAt(i) == "0"){
+        for(i=0; i<4; i++){
+            if(temp.charAt(i) == "0"){
                 loc = setCharAt(loc,i,val.charAt(i));
             }
         }
         return loc;
-
     }else{
         //no overlap, regular assignment
         return val;
     }
 }
-function displayBox() {
+function displayBox(){
     var vCtr = 0;
     var hCtr = 0;
-    for (vCtr = 0; vCtr < cH; vCtr++){
-        for (hCtr = 0; hCtr < cW; hCtr++){
-            if( dict[maMod[hCtr][vCtr]] != undefined){
+    for(vCtr = 0; vCtr < cH; vCtr++){
+        for(hCtr = 0; hCtr < cW; hCtr++){
+            if(dict[maMod[hCtr][vCtr]] != undefined){
                 //if it's a possible character within the limited set, draw
                 document.getElementById('grid_txt').value += dict[maMod[hCtr][vCtr]];
             }else{
                 //otherwise swap out all BOLD for regular LINE
-                //impossibles are: LBBB, BLBB, BBLB, BBBL, and the inverse
+                //impossible for ASCII are: LBBB, BLBB, BBLB, BBBL, and the inverse
                 var temp = maMod[hCtr][vCtr];
                 temp = temp.replace(/B/g, 'L');
                 //put in the temp replacement
@@ -228,36 +235,32 @@ function displayBox() {
     }
 }
 /*--------------------------------------------------*/
-function renderIso(ctxt,img) {
+function renderIso(ctxt,img){
     var rowX = document.getElementById("map").width / 2;
     var rowY = 0;
     var colX = document.getElementById("map").width / 2;
     var colY = 0;
-
+    
     //hardprogrammed sprite dimensions !!!
     cubeHalfWidth = 14;
     cubeHalfHeight = 7;
-
+    
     for(e = 0; e < cH; e+=1){
         rowX = colX
         rowY = colY
         for(i = 0; i < cW; i++){
-            if( maMod[i][e] != "0000"){
-
+            if(maMod[i][e] != "0000"){
+                
                 //only one wall thickness rendered
-                if(maMod[i][e].indexOf('B') === -1)
-                {
+                if(maMod[i][e].indexOf('B') === -1){
                     img.src = "img/" + maMod[i][e] + ".png";
                 }else{
                     img.src = "img/cube.png";
                 }
-
                 
-                //img.onload = function() {
-                    ctxt.drawImage(img, rowX, rowY);
-                //}
+                ctxt.drawImage(img, rowX, rowY);
             }
-
+            
             rowX += cubeHalfWidth;
             rowY += cubeHalfHeight;
         }
